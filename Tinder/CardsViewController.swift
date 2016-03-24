49d565fa3a32ccf3bbd0b6cc8f7b360c82681719
Gary Tragesser <tragessere@gmail.com>
2016-03-24 16:26:29 -0400

    Milestone 2

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class CardsViewController: UIViewController {
+	@IBOutlet weak var profileImageView: UIImageView!
+	
+	var imageStartPosition: CGPoint?
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -20,6 +23,15 @@ class CardsViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-
+	@IBAction func onProfileDragged(gesture: UIPanGestureRecognizer) {
+		let profileImageView = gesture.view as! UIImageView
+		
+		if gesture.state == UIGestureRecognizerState.Began {
+			imageStartPosition = profileImageView.center
+		} else if gesture.state == UIGestureRecognizerState.Changed {
+			let translation = gesture.translationInView(view)
+			profileImageView.center = CGPoint(x: imageStartPosition!.x+translation.x, y: imageStartPosition!.y)
+		}
+	}
 }
 
