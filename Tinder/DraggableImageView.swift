51a7ed6eb908da3d71b215f296703bb085f71fe3
Gary Tragesser <tragessere@gmail.com>
2016-03-24 21:32:15 -0400

    Milestone 4

--- Tinder/DraggableImageView.swift
+++ Tinder/DraggableImageView.swift
@@ -15,13 +15,16 @@ class DraggableImageView: UIImageView {
 		set { self.image = profileImage }
 	}
 	
-	var startPosition: CGPoint?
-	
+	var originalCenter: CGPoint!
+	var startPosition: CGPoint!
+	var touchedOnTopHalf: Bool!
 	
 	override func awakeFromNib() {
 		let panGesture = UIPanGestureRecognizer(target: self, action: #selector(DraggableImageView.onImageDragged(_:)))
 		
 		self.addGestureRecognizer(panGesture)
+		
+		originalCenter = center
 	}
 	
 
@@ -36,9 +39,30 @@ class DraggableImageView: UIImageView {
 	func onImageDragged(gesture: UIPanGestureRecognizer) {
 		if gesture.state == UIGestureRecognizerState.Began {
 			startPosition = self.center
+			
+			let touchPoint = gesture.locationInView(self)
+			
+			if touchPoint.y > (self.frame.height / 2) {
+				touchedOnTopHalf = false
+			} else {
+				touchedOnTopHalf = true
+			}
+			
 		} else if gesture.state == UIGestureRecognizerState.Changed {
 			let translation = gesture.translationInView(self.superview)
-			self.center = CGPoint(x: startPosition!.x+translation.x, y: startPosition!.y)
+			self.center = CGPoint(x: startPosition.x+translation.x, y: originalCenter.y)
+			var offset = (center.x - originalCenter.x) / 4
+			
+			if !touchedOnTopHalf {
+				offset *= -1
+			}
+			
+			let rotation = CGAffineTransformRotate(CGAffineTransformIdentity, degreesToRadians(offset))
+			self.transform = rotation
 		}
+	}
+	
+	func degreesToRadians(angle: CGFloat) -> CGFloat {
+		return angle / 180.0 * CGFloat(M_PI)
 	}
 }
